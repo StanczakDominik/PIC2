045c99535f14d3906c0c1b6b15b3af7cd1a27d6a
Dominik Stańczak <stanczakdominik@gmail.com>
2015-12-27 10:21:47 +0100

    test case for raised, periodic (sin) field

--- interpolation.py
+++ interpolation.py
@@ -23,6 +23,36 @@ def InterpolateElectricField(r, electric_field_grid):
             field_array[i] += (grid[ind+1]-r[i]) * electric_field_grid[ind+1]
     return field_array/dx
 
+def test_raised_sin_field_interpolation():
+    def sin(x, a, b,c,d):
+        return d+a*np.sin(b*x+c)
+    field=np.sin(2*np.pi*grid/L)+5
+    r = distributions.NonRandomUniform(1000)
+    interpolated_field = InterpolateElectricField(r, field)
+    params, covs = curve_fit(sin, r, interpolated_field)
+    a, b, c, d = params
+    da, db, dc, dd = np.sqrt(np.diag(covs**2))
+    total_rel_error = np.sqrt((da/a)**2+(db/b)**2+(dc/c)**2+(dd/d)**2)
+    print(total_rel_error)
+    condition = total_rel_error < 0.0000001
+    if not condition:
+        fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=False)
+        ax1.plot(grid, np.zeros_like(grid), "r.")
+        ax1.plot(r, np.zeros_like(r), "bo")
+        ax1.quiver(r, np.zeros_like(r), np.zeros_like(r), interpolated_field, interpolated_field)
+        ax2.plot(grid, np.zeros_like(grid), "r.")
+        ax2.plot(grid, field, "g-")
+        ax2.plot(r, interpolated_field, "bo--")
+        ax1.set_xlim(0,L)
+        ax1.grid(True)
+        ax2.grid(True)
+        field_sin = sin(r, a, b, c, d)
+        ax2.plot(r, field_sin, "g--")
+        print(a, b, c)
+        print(da, db, dc)
+        plt.show()
+    assert condition
+
 def test_sin_field_interpolation():
     def sin(x, a, b,c):
         return a*np.sin(b*x+c)
