8ae6549d9f640243f6fded2461ac258ac8746c8b
Dominik Stańczak <stanczakdominik@gmail.com>
2015-12-27 09:45:04 +0100

    Removed some unnecessary comments and continuing to zero in on the problem with interpolation

--- interpolation.py
+++ interpolation.py
@@ -4,42 +4,45 @@ import distributions
 from scipy.optimize import curve_fit
 
 
-@jit(nopython=True)
+# @jit(nopython=True)
 def InterpolateElectricField(r, electric_field_grid):
-    r = r % 1
+    # r = r % L
     N = len(r)
     field_array=np.zeros(N)
+    # print(grid, dx)
     for i in range(N):
         ind = int(r[i]//dx)
+        # print(r[i], dx, r[i]/dx, r[i]//dx)
         if ind == NX-1:
+            # TODO: tu jest problem!!!!!
             field_array[i] += (r[i]-grid[ind]) * electric_field_grid[ind]
             field_array[i] += (L-r[i]) * electric_field_grid[0]
         else:
             field_array[i] += (r[i]-grid[ind]) * electric_field_grid[ind]
             field_array[i] += (grid[ind+1]-r[i]) * electric_field_grid[ind+1]
-    return field_array#/dx
+    return field_array/dx
 
 if __name__=="__main__":
     def line(x, a, b):
         return a*x+b
     fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=False)
     field=grid
-    r = distributions.NonRandomUniform(10)
+    r = distributions.NonRandomUniform(1000)
     interpolated_field = InterpolateElectricField(r, field)
     print(interpolated_field)
     ax1.plot(grid, np.zeros_like(grid), "r.")
     ax1.plot(r, np.zeros_like(r), "bo")
-    ax1.quiver(r, np.zeros_like(r), np.zeros_like(r), interpolated_field)
+    ax1.quiver(r, np.zeros_like(r), np.zeros_like(r), interpolated_field, interpolated_field)
     ax2.plot(grid, np.zeros_like(grid), "r.")
     ax2.plot(grid, field, "g-")
-    ax2.plot(r, interpolated_field, "bo")
+    ax2.plot(r, interpolated_field, "bo--")
     ax1.set_xlim(0,L)
     ax1.grid(True)
     ax2.grid(True)
     params, covs = curve_fit(line, r, interpolated_field)
     a, b = params
     field_line = line(r, a, b)
-    ax2.plot(r, field_line, "bo--")
+    ax2.plot(r, field_line, "g--")
     print(a, dx)
     plt.show()
     #fit straightline
